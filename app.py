9687c3200e8c208673f207627e5ec14a6d0c06ec
team-medibot <79132632+team-medibot@users.noreply.github.com>
2021-04-03 14:27:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 @app.route("/bot", methods=["POST"])
 def response():
     query = dict(request.form)['query']
-    return jsonify({"response" : query})
+    (query == "Hi")?return jsonify({"response" : "Hello"}):return null
 if __name__=="__main__":
     app.run(host="0.0.0.0",)
