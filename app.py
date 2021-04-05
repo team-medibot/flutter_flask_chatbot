b0fb1e62265ab1f74e0944278e5149dbfa7b6913
team-medibot <79132632+team-medibot@users.noreply.github.com>
2021-04-05 13:45:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ def response():
         result = "Hello"                                                                                                                                                       
     return jsonify({"response" : result})
 if __name__=="__main__":
-    app.run(host="0.0.0.0",)'''
+    app.run(host="0.0.0.0",)
+'''
 
 
 from flask import Flask, jsonify,request
